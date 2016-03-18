4c01a729f81f414e9d96eb6d717381fb6aa960db
Jessica <a71164@alunos.uminho.pt>
2016-03-18 11:22:47 +0000

    adiciona versao 2 Relatorio + Tutorials 1

---
+++ C#Tutorials/Tutorials/Tutorials/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Hello1.cs
+
+namespace Tutorials{
+    class Program{
+        static void Main(string[] args){
+            System.Console.WriteLine("Hello, World!");
+            Console.WriteLine("You entered the following {0} command line arguments:",
+         args.Length);
+            for (int i = 0; i < args.Length; i++){
+                Console.WriteLine("{0}", args[i]);
+            }
+        }
+    }
+}
+
