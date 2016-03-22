126c88d1385ec9649a2b5d27d983dc0982d954f7
Jessica <a71164@alunos.uminho.pt>
2016-03-22 16:58:23 +0000

    add changes

--- C#Tutorials/Tutorials/Tutorials/Program.cs
+++ C#Tutorials/Tutorials/Tutorials/Program.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// arguments: A B C D
 
 // Hello1.cs
 
-namespace Tutorials{
+namespace Tutorials
+{
     class Program{
         static void Main(string[] args){
+
+            //args = { "banana"; "ana" };
+            //TODO fix this and try to input some arguments 
+
             System.Console.WriteLine("Hello, World!");
             Console.WriteLine("You entered the following {0} command line arguments:",
          args.Length);
