1d0c6252aa29e4d9966f6bbc804e02a543c06cf6
Jessica <a71164@alunos.uminho.pt>
2016-03-23 15:16:39 +0000

    add a struct

--- C#Tutorials/TutorialsMicrosoft0/TutorialsMicrosoft0/Program.cs
+++ C#Tutorials/TutorialsMicrosoft0/TutorialsMicrosoft0/Program.cs
@@ -9,6 +9,50 @@ namespace TutorialsMicrosoft
         private string myName = "N/A";
         private int myAge = 0;
 
+        //Main function for the tutorial basics
+        public static void Main()
+        {
+            System.Console.WriteLine("Hello Console!");
+            //printing to the console
+
+
+            int[] table = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                table[i] = 15;
+            }
+            //arrays and acessing to it.
+
+
+            System.Console.WriteLine("Table array is " + table.Length + " length");
+
+            foreach (int i in table)
+                System.Console.WriteLine(i);
+            //using foreach
+
+            //
+            Console.WriteLine("Simple Properties");
+
+            // Create a new Person object:
+            Program person = new Program();
+
+            // Print out the name and the age associated with the person:
+            Console.WriteLine("Person details - {0}", person);
+
+            // Set some values on the person object:
+            person.Name = "Joe";
+            person.Age = 99;
+            Console.WriteLine("Person details - {0}", person);
+
+            // Increment the Age property:
+            person.Age += 1;
+            Console.WriteLine("Person details - {0}", person);
+
+            SimpleStruct ss = new SimpleStruct();
+            ss.X = 5;
+            ss.DisplayX();
+        }
+
         // Declare a Name property of type string:
         public string Name
         {
@@ -40,49 +84,39 @@ namespace TutorialsMicrosoft
             return "Name = " + Name + ", Age = " + Age;
         }
 
-        //Main function for the tutorial basics
-        public static void Main()
-        {
-            System.Console.WriteLine("Hello Console!");
-            //printing to the console
-
-
-            int[] table = new int[10];
-            for (int i = 0; i < 10; i++)
-            {
-                table[i] = 15;
-            }
-            //arrays and acessing to it.
-
-
-            System.Console.WriteLine("Table array is " + table.Length + " length");
-
-            foreach (int i in table)
-             System.Console.WriteLine(i);
-            //using foreach
-
-            //
-            Console.WriteLine("Simple Properties");
-
-            // Create a new Person object:
-            Program person = new Program();
-
-            // Print out the name and the age associated with the person:
-            Console.WriteLine("Person details - {0}", person);
-
-            // Set some values on the person object:
-            person.Name = "Joe";
-            person.Age = 99;
-            Console.WriteLine("Person details - {0}", person);
-
-            // Increment the Age property:
-            person.Age += 1;
-            Console.WriteLine("Person details - {0}", person);
-        }
+        
 
 
 
     }
+
+
+
+    struct SimpleStruct
+    {
+        private int xval;
+        public int X
+        {
+            get
+            {
+                return xval;
+            }
+            set
+            {
+                if (value < 100)
+                    xval = value;
+            }
+        }
+        public void DisplayX()
+        {
+            Console.WriteLine("The stored value is: {0}", xval);
+        }
+    }
+
+   
+
+
+
 
     //TODO https://msdn.microsoft.com/en-us/library/aa288479(v=vs.71).aspx
 }
