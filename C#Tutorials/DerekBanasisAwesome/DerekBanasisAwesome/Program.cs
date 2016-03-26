b3326c6cf272cbe5d8f02ff0e77fd0bbd6d53e01
Jessica <a71164@alunos.uminho.pt>
2016-03-26 10:15:32 +0000

    add first two lessons groups Derek Banas C#

---
+++ C#Tutorials/DerekBanasisAwesome/DerekBanasisAwesome/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerekBanasisAwesome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var anotherName = "Tom";
+            Console.WriteLine("amotherName is a {0}", anotherName.GetTypeCode());
+
+            //Casting
+            double pi = 3.14;
+            int intPi = (int)pi;
+
+            //Math Functions
+            //Acos, Asim, Atan, Cos, Cosh, 
+            /* 
+            Math.Abs Math.Ceiling Math.Floor Math.Max  Math.Pow  Math.Round  Math.Sqrt
+            */
+
+            //Random numbers
+            Random rand = new Random();
+            Console.WriteLine("Random between 1 and 10" + (rand.Next(1, 11)));
+
+            //Relational Operatores : > < >= <= == != 
+            //Logical Operators: && || ^ !  
+
+            int age = 17;
+            if ((age >= 5) && age <= 7)
+            {
+                Console.WriteLine("Go to elementary school");
+            }
+            else if (age > 7 && age < 13)
+            {
+                Console.WriteLine("Go to middle school");
+            }
+            else {
+                Console.WriteLine("Go to hight school");
+            }
+
+            Console.WriteLine("! true = " + !true);
+
+            bool canDrive = (age >= 16) ? true : false;
+
+            switch (age)
+            {
+                case 0:
+                    Console.WriteLine("Infant");
+                    break;
+
+                case 1: Console.WriteLine(); break;
+
+                case 2:
+                    Console.WriteLine("Toddler");
+                    goto Cute;
+
+                default:
+                    Console.WriteLine("noting");
+                    break;
+            }
+
+
+
+        Cute:
+            Console.WriteLine("Toddlers are Cute");
+
+
+            //Go to statements can cause nightmares.
+
+
+            /* Looping  */
+
+            int i = 0;
+
+            while (i < 10)
+            {
+                if (i == 7)
+                {
+                    i++; continue; //continue makes the program ignore all the code thats after this piece of code. IT will increment to 8 and  go back to the loop.
+                }
+
+                if (i == 9) break;
+                //It will get out of the loop
+
+                if ((i % 2) > 0)
+                {
+                    Console.WriteLine(i);
+                } //It will only print odd numbers...
+                i++;
+            }
+
+
+                string guess;
+                do
+                {
+                    Console.WriteLine("Guess a Number");
+                    guess = Console.ReadLine();
+                } while (!guess.Equals("15"));
+
+                //It will do something at least once, before checking the condition inside the while loop.
+
+                for (int iu = 0; iu < 10; iu++)
+                {
+                    if ((iu % 2) > 0)
+                        Console.WriteLine(iu);
+                }
+
+                //foreach used in collections
+                string randStr = "Here are some random characters";
+                foreach (char c in randStr)
+                {
+                    Console.WriteLine(c);
+                }
+
+            }
+        }
+    }
---
+++ C#Tutorials/StringOperators/StringOperators/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sampString = "A bunch of random words";
+            Console.WriteLine("Is empty: " + String.IsNullOrEmpty(sampString));
+            Console.WriteLine("Is empty: " + String.IsNullOrWhiteSpace(sampString));
+            Console.WriteLine("String Length: " + sampString.Length);
+
+            Console.WriteLine("Index of bunch " + sampString.IndexOf("bunch"));
+            Console.WriteLine("2nd Word " + sampString.Substring(2, 6));
+
+            string sampString2 = "More random words";
+            Console.WriteLine("Strings Equal :" + sampString.Equals(sampString2));
+            Console.WriteLine("Starts with \"A bunch\"" + sampString.StartsWith("A bunch"));
+            Console.WriteLine("Ends with words  " + sampString.EndsWith("words"));
+
+
+            sampString = sampString.Trim(); //TrimEnd TrimStart
+            Console.WriteLine(sampString);
+
+            sampString = sampString.Replace("words", "characters");
+            Console.WriteLine(sampString);
+
+            sampString = sampString.Remove(0, 2);
+            Console.WriteLine(sampString);
+
+            string[] names = new string[3] { "Matt", "Joe", "Paul" };
+
+            Console.WriteLine("Name List " + String.Join(",", names));
+            string fmrStr = String.Format("{0:c} {1:00.00} {2:%.00} {3:0,0}", 1.56, 15.567, .56, 1000);
+            Console.WriteLine(fmrStr);
+
+            /* String Builder */
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("This is the first sentence");
+            sb.AppendFormat("My name is {0} and I live in {1} ", "Derek", "Pennsylvania");
+            //sb.Clear();
+            sb.Replace("a", "e");
+
+            sb.Remove(5, 7); //from 5 til not included 7 
+
+            Console.WriteLine(sb.ToString());
+
+
+
+            /* Arrays */
+
+            int[] randNumArray;
+
+            int[] randArray = new int[10];
+            int[] randArray2 = { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Array Length " + randArray2.Length);
+            Console.WriteLine("Item 0" + randArray2[0] );
+
+            for(int i = 0; i < randArray2.Length; i++)
+            {
+                Console.WriteLine("{0} : {1}", i, randArray2[i]);
+            }
+
+            foreach(int num in randArray2)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine("Where is 1 " + Array.IndexOf(randArray2, 1));
+            string[] namess = { "Tom", "Ron", "Jessica" };
+            string nameStr = string.Join(",", namess);
+            string[] nameArray = nameStr.Split(',');
+
+
+            int[,] multArray = new int[5, 3];
+            int[,] multArray2 = { { 0, 1 }, { 2, 3 }, { 4, 5 } };
+
+            foreach(int num in multArray2)
+            {
+                Console.WriteLine(num);
+            }
+
+            for(int x=0; x<multArray2.GetLength(0); x++)
+            {
+                for(int y=0; y< multArray2.GetLength(1); y++)
+                {
+                    Console.WriteLine("{0} | {1} : {2}", x, y, multArray2[x,y]);
+                }
+            }
+        }
+    }
+}
