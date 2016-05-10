7fc1a003997506b163eaa651f797d407a7f27cab
Jessica <a71164@alunos.uminho.pt>
2016-05-10 16:22:32 +0100

    adicionar clone, toString e equal a Missao

--- 3Fase/codigo_v1/Assistente_Espacial/Missao.java
+++ 3Fase/codigo_v1/Assistente_Espacial/Missao.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.Date;
-
+import java.util.GregorianCalendar;
 public class Missao
 {
     private String nome;
@@ -8,8 +8,8 @@ public class Missao
     private String nomeArea;
     private Plano plano;
     private ArrayList<String> factos;
-    private Date datainicio;
-    private Date datafim;
+    private GregorianCalendar datainicio;
+    private GregorianCalendar datafim;
     //TODO check date
     //TODO ver se temos factos no diagrama de dominio
     
@@ -22,15 +22,15 @@ public class Missao
         this.nomeArea =" ";
         this.plano= null;
         this.factos = null;
-        this.datainicio = new Date();
-        this.datafim = new Date();
+        this.datainicio = new GregorianCalendar();
+        this.datafim = new GregorianCalendar();
     
     }
     
     /**
      * Construtor parametrico
      */
-     public Missao(String nome, float area, String nomeArea, Plano plano, Date d1, Date d2){
+     public Missao(String nome, float area, String nomeArea, Plano plano, GregorianCalendar d1, GregorianCalendar d2){
         this.nome = nome;
         this.areatotal = area;
         this.nomeArea = nomeArea;
@@ -75,12 +75,62 @@ public class Missao
         return this.plano.clone();
     }
     
-    public Date getDataI(){
+    public GregorianCalendar getDataI(){
         return this.datainicio;
     }
     
-    public Date getDataF(){
+    public GregorianCalendar getDataF(){
         return this.datafim;
+    }
+    
+    //Clone, equals, toString
+    
+    public Missao clone(){
+        return new Missao(this);
+    }
+    
+    public boolean equals(Missao m){
+        if(this==m) return true;
+        if(( m==null ) || (m.getClass() != this.getClass()) ) return false;
+        
+        Missao aux = (Missao) m;
+        boolean comp = (this.areatotal == m.getArea());
+        comp = comp && (this.nome.equals(m.getNome()));
+        comp = comp && (this.datainicio.equals(m.getDataI()));
+        comp = comp && (this.datafim.equals(m.getDataF()));
+        //comp = comp && ( this.plano.equals(m.getPlano()) );
+        
+        return comp;
+    
+    }
+    
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        String areatotal_;
+        String day1, month1, year1, day2, month2, year2;
+        
+        day1 = String.valueOf(datainicio.get(GregorianCalendar.DAY_OF_MONTH));
+        month1 = String.valueOf(datainicio.get(GregorianCalendar.MONTH));
+        year1 = String.valueOf(datainicio.get(GregorianCalendar.YEAR));
+       
+          day2 = String.valueOf(datafim.get(GregorianCalendar.DAY_OF_MONTH));
+        month2 = String.valueOf(datafim.get(GregorianCalendar.MONTH));
+        year2 = String.valueOf(datafim.get(GregorianCalendar.YEAR));
+       
+        sb.append("Missao " + nome + ";");
+        sb.append("Data Inicio: " + day1 + "/" + month1 + "/" + year1 +"\n");
+        sb.append("Data Fim: " + day1 + "/" + month1 + "/" + year1 +"\n");
+        return sb.toString();
+        
+        /*
+         *  private String nome;
+    private float areatotal;
+    private String nomeArea;
+    private Plano plano;
+    private ArrayList<String> factos;
+    private Date datainicio;
+    private Date datafim;
+         */
     }
     
     
