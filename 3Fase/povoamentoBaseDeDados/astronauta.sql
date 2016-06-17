b97a015aaa7dbb0557e651212a2a7272470b1341
Jessica <a71164@alunos.uminho.pt>
2016-06-17 19:00:10 +0100

    adiciona resumo requisitos + rascunho povoacao astronauta

---
+++ 3Fase/povoamentoBaseDeDados/astronauta.sql
@@ -0,0 +1,20 @@
+
+USE ---;
+SET SQL_SAFE_UPDATES = 0;
+
+INSERT INTO astronauta
+(id_Astronauta, NomeAstronauta, Cargo)
+VALUES
+(1,'Maria Helena', 'gestor_missao'),
+(2,'Pedro Cunha', 'especialista_rochas'),
+(3,'Pedro Alves', 'médico'),
+(4,'Joana Arantes', 'cartografo'),
+(5,'Daniel Malhadas', 'geólogo'),
+(6,'Jessica Pereira', 'gestor_missao'),
+(7,'Rita Rua', 'geologo'),
+(8,'Susana Mendes', 'cartografo'),
+(9,'Maria Pereira', 'especialista_rochas'),
+(10,'Marcos Pereira', 'medico'),
+(11,'Laura Mendes', 'gestor_missao');
+
+SET SQL_SAFE_UPDATES = 1;
